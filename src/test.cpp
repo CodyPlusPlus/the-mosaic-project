4481beacc2424a4d85eb0c7c203a62dbaea5c845
Brady Pascoe <bpas247@hotmail.com>
2018-11-26 15:39:50 -0800

    pause console on output

--- src/test.cpp
+++ src/test.cpp
@@ -1,8 +1,8 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_RUNNER  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "print.hpp"
 #include "mosaic.h"
-
+#include <iostream>
 
 
 TEST_CASE("Prints hello world") {
@@ -17,3 +17,8 @@ TEST_CASE("Tests mosaic function")
 	cv::imwrite("outorig.jpg", test);
 }
 
+int main(int argc, char ** argv) {
+	int result = Catch::Session().run(argc, argv);
+	system("PAUSE");
+	return result;
+}
