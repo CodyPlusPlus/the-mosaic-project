b017ca6ca99b89ef1ee87ebdb87f6e262e439f40
Brady Pascoe <bpas247@hotmail.com>
2018-11-26 22:43:38 -0800

    add more tree tests

--- src/test.cpp
+++ src/test.cpp
@@ -28,11 +28,31 @@ TEST_CASE("Tests Tree Constructor") {
 
 	std::vector<cv::Mat> treeRoot = testTree.getRoot()->data;
 
-	REQUIRE(treeRoot.size() > 0);
+	REQUIRE(treeRoot.size() == 1);
 
 	cv::Mat fromTree = treeRoot[0];
 
 	REQUIRE((test.rows == fromTree.rows && test.cols == fromTree.cols));
+}
+
+TEST_CASE("Tree Add Function") {
+	cv::Mat test(5, 5, CV_8U), test2(75, 74, CV_8U); // Values should all default to 0
+
+	Tree testTree(test);
+
+	testTree.addImage(test2);
+
+	std::vector<cv::Mat> treeRoot = testTree.getRoot()->data;
+
+	REQUIRE(treeRoot.size() == 2);
+
+	cv::Mat fromTree = treeRoot[0];
+
+	REQUIRE((test.rows == fromTree.rows && test.cols == fromTree.cols));
+
+	cv::Mat fromTree2 = treeRoot[1];
+
+	REQUIRE((test2.rows == fromTree2.rows && test2.cols == fromTree2.cols));
 }
 
 int main(int argc, char ** argv) {
